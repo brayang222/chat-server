80e877752db65398b3ae6b37b40283b93fc07e56
brayang222 <brayangomez521@gmail.com>
2025-06-20 23:24:04 -0500

    feat: add user retrieval by username and configure Vercel deployment

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 import usersModel from "../models/users.js";
+import { generateToken } from "../helpers/auth.js";
 
 class usersController {
   constructor() {}
@@ -30,6 +31,17 @@ class usersController {
       res.status(200).json(data);
     } catch (error) {
       console.error(first);
+    }
+  }
+
+  async getUserByUsername(req, res) {
+    const { username } = req.params;
+
+    try {
+      const user = await usersModel.getByUsername(username);
+      res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
     }
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const route = express.Router();
 
 route.post("/login", usersController.login);
 route.get("/", usersController.getUsers);
+route.get("/:username", usersController.getUserByUsername);
 
 export default route;
